10fd9e53fea0de5d60594cf4a5ff6fff7ddeccfb
Steven Yap <stevenyap@futureworkz.com>
2015-07-21 09:23:29 +0700

    Improve user-friendliness of passgen bash script

--- bin/helpers.bash
+++ bin/helpers.bash
@@ -4,4 +4,4 @@ alias ll='ls -al'
 alias ls='ls -GFh'
 
 # generate password
-alias passgen="openssl rand -base64 ${1:-10} | tee >(tr -d '\n' | pbcopy)"
+alias passgen="openssl rand -base64 ${1:-10} | tr -d '\n' | tee >(pbcopy) && echo ' is copied to your clipboard'"
