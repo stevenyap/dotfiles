aab54e0987075004f4094b3971e2bc6d70132be1
Steven Yap <stevenyap@futureworkz.com>
2015-09-04 14:30:29 +0700

    Add lock screen alias

--- bin/helpers.bash
+++ bin/helpers.bash
@@ -5,3 +5,6 @@ alias ls='ls -GFh'
 
 # generate password
 alias passgen="openssl rand -base64 ${1:-10} | tr -d '\n' | tee >(pbcopy) && echo ' is copied to your clipboard'"
+
+# lock the screen
+alias lk='/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend'
