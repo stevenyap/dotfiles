7c190af8c4a54a4dc93944b1f45590c190032924
Steven Yap <stevenyap@futureworkz.com>
2019-03-11 11:46:28 +0800

    Add colors to ls

--- bin/helpers.bash
+++ bin/helpers.bash
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
+# Custom ll and ls
+export LSCOLORS=gxfxbEaEBxxEhEhBaDaCaD
 alias ll='ls -al'
 alias ls='ls -GFh'
 
 # generate password
-alias passgen="openssl rand -base64 ${1:-10} | tr -d '\n' | tee >(pbcopy) && echo ' is copied to your clipboard'"
-
-# lock the screen
-alias lk='/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend'
+alias generatepassword="openssl rand -base64 ${1:-10} | tr -d '\n' | tee >(pbcopy) && echo ' is copied to your clipboard'"
